becf00d804a1a45e45aaad69bed392d2c1e313e1
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-09-06 20:42:01 +0300

    11

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -15,14 +19,23 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+//        val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        binding = DataBindingUtil.inflate(inflater,R.layout.home_fragment,container,false)
+        binding.viewModel = viewModel
 
-        return view
+        viewModel.navigateToSearch.observe(this, Observer {
+            if (it) {findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+            viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
